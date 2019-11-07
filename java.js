7f8be392978155d7eb74a4314eacd9a807c2b41f
jimmy nagles <jimmynagles@gmail.com>
2019-11-07 14:53:18 -0800

    basic page laidout

---
+++ java.js
@@ -0,0 +1,21 @@
+var gifsApi = KmTJWFHfw7I9gCj0RMZL7kRHYpuAI7xz;
+$.ajax({
+url: gifsApi,
+method: "GET"
+}).then(function(add){
+
+
+    
+
+});
+
+//gifs 
+$("#images")
+//buttons
+$("#botones")
+//search input
+$("#search")
+
+
+
+var botones = $("<button>")
